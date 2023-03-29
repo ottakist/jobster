a292ed5c3a5b6e4c9da3d4f0e8c784ec65895146
ottakist <102596775+ottakist@users.noreply.github.com>
2023-03-29 19:11:04 +0300

    stats styled final

--- src/assets/wrappers/StatsContainer.js
+++ src/assets/wrappers/StatsContainer.js
@@ -5,18 +5,5 @@ const Wrapper = styled.section`
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: space-around;
-
-  @media (max-width: 767px) {
-    /* grid-template-columns: 2rem 2rem 2rem;
-    height: 2rem; */
-  }
-  @media (min-width: 768px) {
-    /* grid-template-columns: 1fr 1fr;
-    column-gap: 1rem; */
-  }
-  @media (min-width: 1120px) {
-    /* grid-template-columns: 1fr 1fr 1fr;
-    column-gap: 1rem; */
-  }
 `;
 export default Wrapper;
